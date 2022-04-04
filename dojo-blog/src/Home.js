4ee53409ef85d304f3095e6a73a90ba0473991a0
mainoo-smith <mainoo41@gmail.com>
2022-04-03 23:51:35 -0600

    Added new files

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,38 @@
+import {useEffect, useState} from 'react';
+import BlogList from './BlogLists';
+
 const Home = () => {
+    // handle click event
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(response => {
+            if(!response.ok){
+                throw Error("resource not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, []);
+
+    // use conditional and (&&) to make sure data (blogs) 
+    // is present before rendering component
     return ( 
         <div className="home">
-            <h1>Home Page</h1>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} />}
         </div>
      );
 }
